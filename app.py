a81bca965a0ead3ea272c9d3d1ce3bc560c1f38e
savaseiv-create <savaseiv@gmail.com>
2026-03-26 07:56:17 +0000

    Fix indentation in hello function definition

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/') 
-def hello():
+def hello(): 
     if 'use_template' in request.args:
         return render_template('hello.html')
     else:
